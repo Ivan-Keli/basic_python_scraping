80c45bbf5f647193562410a9a71da9f9f63f301b
Ivan Mwendwa Keli <116259292+Ivan-Keli@users.noreply.github.com>
2024-03-09 17:24:14 +0300

    Update scraping_logins_selenium.py
    Code Update

--- scraping_logins_selenium.py
+++ scraping_logins_selenium.py
@@ -1,17 +1,18 @@
-#INSTALL SELENIUM BEFORE RUNNING THIS CODE
-#pip3 install selenium
-from selenium.webdriver.chrome.service import Service
+import csv
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import csv
 import time
 import getpass
+from selenium.common.exceptions import NoSuchElementException
+import chromedriver_autoinstaller  # Add this line
 
-#IF USING A RASPBERRY PI, FIRST INSTALL THIS OPTIMIZED CHROME DRIVER
-#sudo apt-get install chromium-chromedriver
-browser_driver = Service('/usr/lib/chromium-browser/chromedriver')
-page_to_scrape = webdriver.Chrome(service=browser_driver)
-page_to_scrape.get("https://quotes.toscrape.com")
+# Automatically download and install chromedriver
+chromedriver_autoinstaller.install()
+
+# Set up the Chrome driver
+browser_driver = webdriver.Chrome()
+page_to_scrape = browser_driver
+page_to_scrape.get("http://quotes.toscrape.com")
 
 page_to_scrape.find_element(By.LINK_TEXT, "Login").click()
 
@@ -19,8 +20,6 @@ time.sleep(3)
 username = page_to_scrape.find_element(By.ID, "username")
 password = page_to_scrape.find_element(By.ID, "password")
 username.send_keys("admin")
-#USING GETPASS WILL PROMPT YOU TO ENTER YOUR PASSWORD INSTEAD OF STORING
-#IT IN CODE. YOU'RE ALSO WELCOME TO USE A PYTHON KEYRING TO STORE PASSWORDS.
 my_pass = getpass.getpass()
 password.send_keys(my_pass)
 page_to_scrape.find_element(By.CSS_SELECTOR, "input.btn-primary").click()
@@ -28,12 +27,18 @@ page_to_scrape.find_element(By.CSS_SELECTOR, "input.btn-primary").click()
 quotes = page_to_scrape.find_elements(By.CLASS_NAME, "text")
 authors = page_to_scrape.find_elements(By.CLASS_NAME, "author")
 
-file = open("scraped_quotes2.csv", "w")
+file = open("scraped_quotes.csv", "w", newline="", encoding="utf-8")
 writer = csv.writer(file)
 
 writer.writerow(["QUOTES", "AUTHORS"])
-
-for quote, author in zip(quotes, authors):
-    print(quote.text + " - " + author.text)
-    writer.writerow([quote.text, author.text])
+while True:
+    quotes = page_to_scrape.find_elements(By.CLASS_NAME, "text")
+    authors = page_to_scrape.find_elements(By.CLASS_NAME, "author")
+    for quote, author in zip(quotes, authors):
+        print(quote.text + " - " + author.text)
+        writer.writerow([quote.text, author.text])
+    try:
+        page_to_scrape.find_element(By.PARTIAL_LINK_TEXT, "Next").click()
+    except NoSuchElementException:
+        break
 file.close()
